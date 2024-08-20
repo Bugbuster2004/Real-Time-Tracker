f87a520951254e0b55b53639328dba8f1991d486
Bugbuster2004 <siddhant6338bansal@gmail.com>
2024-08-21 00:20:15 +0530

    finalized tracker app more features to be added now

--- app.js
+++ app.js
@@ -15,6 +15,17 @@ app.use(express.static("public"));
 
 //handling reequest from frontend
 io.on("connection", function (socket) {
+  //accept the send-location event in backend
+  socket.on("send-location", function (data, username) {
+    //emit the location to all connected clients send the req to frontend
+    io.emit("recieve-location", { id: socket.id, ...data, username });
+  });
+  //handle disconnection feature
+  socket.on("disconnect", function () {
+    // console.log("a user disconnected");
+    io.emit("user-disconnected", socket.id);
+  });
+
   console.log("new connection");
 });
 
--- public/js/script.js
+++ public/js/script.js
@@ -2,6 +2,8 @@
 
 //this sends a connection request to the backend app.js
 const socket = io();
+const username = prompt("Enter your username");
+
 console.log("hey");
 
 //check if the browser supports the geolocation
@@ -27,7 +29,37 @@ if (navigator.geolocation) {
 }
 
 //now rendering location from leaflet map basically asking google for accessing the location
-const map = L.map("map").setView([0, 0], 10);
+const map = L.map("map").setView([0, 0], 5);
 
 // now to see the actual map we need to write this comman with url learn this
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+
+//adding the markers now hat pin type location
+const markers = {};
+
+//recieving the location here from backend
+//this part basically aligns your map according to the location you are currently using
+socket.on("recieve-location", (data) => {
+  const { id, latitude, longitude } = data;
+  map.setView([latitude, longitude]);
+  if (markers[id]) {
+    markers[id].setLatLng([latitude, longitude]);
+  } else {
+    //add marker
+    console.log("Adding marker");
+    markers[id] = L.marker([latitude, longitude])
+      .addTo(map)
+      .bindPopup(username)
+      .openPopup();
+  }
+});
+
+//handle disconnceton feature
+socket.on("user-disconnected", (id) => {
+  //remove the marker from the map
+  if (markers[id]) {
+    console.log("inside if");
+    map.removeLayer(markers[id]);
+    delete markers[id];
+  }
+});
